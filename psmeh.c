cb984ea3fbb991867759d8eb30347ef0b256741c
David MOLINARI <contact@davidmolinari.fr>
2018-05-23 11:24:41 +0200

    add zombie

--- psmeh.c
+++ psmeh.c
@@ -15,17 +15,17 @@ int main(){
 
   printf("Pere et fils\n" );
   if(pid == 0){
-          while(1){
-            printf("Fils, Pid : %d, PPID : %d, global : %d\n",getpid(), getppid(), global++ );
-      sleep(1);
-    }
+    //while(1){
+      //printf("Fils, Pid : %d, PPID : %d, global : %d\n",getpid(), getppid(), global++ );
+      //sleep(1);
+      exit(0);
 
   }else{
-    //while(1){
-      //printf("Pere, Pid : %d, PPID : %d, global : %d\n",getpid(), getppid(), global );
-      //sleep(1);
-    //}
-    exit(0);
+    while(1){
+    printf("Pere, Pid : %d, PPID : %d, global : %d\n",getpid(), getppid(), global );
+    sleep(1);
+    }
+    //exit(0);
   }
   exit(0);
 
