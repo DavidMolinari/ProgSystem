38b1b7a834f1b5024fe1d7faf583c47a15a92818
David MOLINARI <contact@davidmolinari.fr>
2018-05-24 14:00:00 +0530

    exit shell

--- simpleShell.c
+++ simpleShell.c
@@ -8,22 +8,23 @@ int main(){
   pid_t pid, pidfils;
   int retour;
   char buffer[1024];
-  printf("[GOGOGO]>");
+  while(strcasecmp(buffer, "exit") != 0){
+  printf("[prompt]>");
   fgets(buffer, 1024, stdin);
   if(buffer[strlen(buffer)-1] == '\n')
     buffer[strlen(buffer)-1 ]= '\0';
-
   switch (pid = fork()) {
     case (pid_t)-1:
       perror("SRY MARCHE PAS . . .");
       exit(2);
     case (pid_t) 0:
       execlp(buffer, buffer, NULL);
-      fprintf(stderr, "CONNAIT PAS ... \n");
+      if(strcasecmp(buffer, "exit") != 0)
+        fprintf(stderr, "CONNAIT PAS ... \n");
       exit(1);
     default:
       pidfils = wait(&retour);
   }
-
+}
 
   }
