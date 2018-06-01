3a1271cea4a4fb1ad293c3fab6c67b6277156c84
David MOLINARI <contact@davidmolinari.fr>
2018-06-01 10:56:26 +0200

    Add Train.c

--- MultiThreads/oldcvsimple.c
+++ MultiThreads/oldcvsimple.c
@@ -14,9 +14,6 @@
 
 #include <stdio.h>
 #include <pthread.h>
-#include <time.h>
-#include <sys/time.h>
-#include <errno.h>
 
 #define NB_THREADS  3
 #define NB_INCR 4
@@ -26,19 +23,14 @@ int     compte = 0;
 int     thread_ids[3] = {0,1,2};
 pthread_mutex_t verrou_compte=PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t condition_seuil=PTHREAD_COND_INITIALIZER;
-struct timespec timeToWait;
-struct timeval now;
+
 
 void *incremente_compte(void *idp)
 {
-  // if(idp == (void *)&thread_ids[0]) sleep(5);
-
-  // while(compte <= SEUIL){
-
   int *my_id = idp;
   int i;
+  if(idp == (void *)&thread_ids[0]) sleep(3);
 
-  if(idp == (void *)&thread_ids[1]) sleep(5);
   for (i=0; i<NB_INCR; i++) {
     pthread_mutex_lock(&verrou_compte);
     compte++;
@@ -47,34 +39,57 @@ void *incremente_compte(void *idp)
     if (compte == SEUIL) {
       printf("incremente_compte() : Thread %d, compte %d, signale condition\n", *my_id, compte);
       pthread_cond_signal(&condition_seuil);
-      // return(NULL);
     }
     pthread_mutex_unlock(&verrou_compte);
   }
 
   return(NULL);
 }
-// }
 
+/* Exemple
+gettimeofday(&now);
+timeout.tv_sec = now.tv_sec + 5;
+timeout.tv_nsec = now.tv_usec * 1000;
+retcode = 0;
+while (x <= y && retcode != ETIMEDOUT) {
+    retcode = pthread_cond_timedwait(&cond, &mut, &timeout);
+}
+if (retcode == ETIMEDOUT) {
+  timeout
+} else {
+     agir sur x et y
+}
+pthread_mutex_unlock(&mut);
+*/
 void *attente_seuil(void *idp)
 {
-
-  time_t T;
-  struct timespec t;
-  time(&T);
-
-
   int *my_id = idp;
 
-  printf("idP Thread no 3 : %d\n",idp );
   printf("attente_seuil(): thread %d\n", *my_id);
+
   pthread_mutex_lock(&verrou_compte);
-  printf ("attente_seuil() : attente signalisation de la condition\n");
-    while(pthread_cond_timedwait(&condition_seuil, &verrou_compte, &t) != ETIMEDOUT){
-      pthread_cond_timedwait(&condition_seuil, &verrou_compte, &t);
-    }
+
+  struct timeval now;
+  struct timespec timeout;
+
+  int rt;
+  gettimeofday(&now,NULL);
+
+
+  timeToWait.tv_sec = now.tv_sec+5;
+  timeToWait.tv_nsec = (now.tv_usec+1000UL*300)*1000UL;
+
+
+  while (rc != ETIMEDOUT) {
+    printf ("attente_seuil() : attente signalisation de la condition\n");
+    rc = pthread_cond_timedwait(&condition_seuil, &verrou_compte; &timeout);
     printf("attente_seuil() : thread %d, compte %d condition recue\n", *my_id, compte);
+  }
+
+  if(rc == ETIMEDOUT) printf("TIMED OUT\n");
+
   pthread_mutex_unlock(&verrou_compte);
+
   printf("attente_seuil(): fin du thread %d\n", *my_id);
 
   return(NULL);
@@ -88,6 +103,7 @@ main(void)
   pthread_create(&threads[0], NULL, incremente_compte, (void *)&thread_ids[0]);
   pthread_create(&threads[1], NULL, incremente_compte, (void *)&thread_ids[1]);
   pthread_create(&threads[2], NULL, attente_seuil, (void *)&thread_ids[2]);
+
   for (i = 0; i < NB_THREADS; i++) {
     pthread_join(threads[i], NULL);
   }
--- MultiThreads/prgm/cvsimple.c
+++ MultiThreads/prgm/cvsimple.c
@@ -8,15 +8,12 @@
  ********************************************************
  *
  * cvsimple1.c
- * adaptï¿½ et modifiï¿½ F. Touchard
+ * adapté et modifié F. Touchard
  *
  */
 
 #include <stdio.h>
 #include <pthread.h>
-#include <time.h>
-#include <sys/time.h>
-#include <errno.h>
 
 #define NB_THREADS  3
 #define NB_INCR 4
@@ -24,57 +21,46 @@
 
 int     compte = 0;
 int     thread_ids[3] = {0,1,2};
-pthread_mutex_t verrou_compte=PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t condition_seuil=PTHREAD_COND_INITIALIZER;
-struct timespec timeToWait;
-struct timeval now;
+pthread_mutex_t verrou_compte=PTHREAD_MUTEX_INITIALIZER; 
+pthread_cond_t condition_seuil=PTHREAD_COND_INITIALIZER; 
+
 
 void *incremente_compte(void *idp)
 {
-  // if(idp == (void *)&thread_ids[0]) sleep(5);
-
-  // while(compte <= SEUIL){
-
   int *my_id = idp;
   int i;
 
-  if(idp == (void *)&thread_ids[1]) sleep(5);
   for (i=0; i<NB_INCR; i++) {
     pthread_mutex_lock(&verrou_compte);
     compte++;
-    printf("incremente_compte() : thread %d, compte = %d\n",
+    printf("incremente_compte() : thread %d, compte = %d\n", 
 	   *my_id, compte);
     if (compte == SEUIL) {
       printf("incremente_compte() : Thread %d, compte %d, signale condition\n", *my_id, compte);
       pthread_cond_signal(&condition_seuil);
-      // return(NULL);
     }
     pthread_mutex_unlock(&verrou_compte);
   }
-
+  
   return(NULL);
 }
-// }
 
 void *attente_seuil(void *idp)
 {
-
-  time_t T;
-  struct timespec t;
-  time(&T);
-
-
   int *my_id = idp;
 
-  printf("idP Thread no 3 : %d\n",idp );
   printf("attente_seuil(): thread %d\n", *my_id);
+  
   pthread_mutex_lock(&verrou_compte);
-  printf ("attente_seuil() : attente signalisation de la condition\n");
-    while(pthread_cond_timedwait(&condition_seuil, &verrou_compte, &t) != ETIMEDOUT){
-      pthread_cond_timedwait(&condition_seuil, &verrou_compte, &t);
-    }
+
+  if (compte < SEUIL) {
+    printf ("attente_seuil() : attente signalisation de la condition\n");
+    pthread_cond_wait(&condition_seuil, &verrou_compte);
     printf("attente_seuil() : thread %d, compte %d condition recue\n", *my_id, compte);
+  }
+
   pthread_mutex_unlock(&verrou_compte);
+
   printf("attente_seuil(): fin du thread %d\n", *my_id);
 
   return(NULL);
@@ -88,9 +74,12 @@ main(void)
   pthread_create(&threads[0], NULL, incremente_compte, (void *)&thread_ids[0]);
   pthread_create(&threads[1], NULL, incremente_compte, (void *)&thread_ids[1]);
   pthread_create(&threads[2], NULL, attente_seuil, (void *)&thread_ids[2]);
+
   for (i = 0; i < NB_THREADS; i++) {
     pthread_join(threads[i], NULL);
   }
 
   return 0;
 }
+
+
