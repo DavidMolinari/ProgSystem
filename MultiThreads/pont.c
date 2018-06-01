c1fc35764682354b016c3a0acaec4e89c94fff05
David MOLINARI <contact@davidmolinari.fr>
2018-06-01 11:59:54 +0200

    todo

--- MultiThreads/pont.c
+++ MultiThreads/pont.c
@@ -1,4 +1,3 @@
-
 #include <time.h>
 #include <pthread.h>
 #include <semaphore.h>
@@ -6,118 +5,47 @@
 #include <stdio.h>
 
 
-/* Les villes */
-#define A 0
-#define B 1
-#define C 2
-#define D 3
-
-/* 3 sections critiques , 3 mutex  */
-pthread_mutex_t AB;
-pthread_mutex_t BC;
-pthread_mutex_t DB;
+sem_t mut;
+/* charge max du point */
+#define CHARGEMAX 15
+/* Poids Camion */
+#define PC 15
+/* Poids voiture */
+#define PV 5
 
 
-/* Code des threads */
-void* train_de_A_vers_C(void* arg);
-void* train_de_D_vers_A(void* arg);
 
-int tirage_aleatoire(double max);
-void attendre(double max);
+ void TODO(){};
 
-/* Moniteur : gestion du chemin de fer  */
-void utiliser_le_segment(int ville_depart, int ville_arrivee){
-  if(ville_depart == A)
-    pthread_mutex_lock(&AB);
-  if(ville_depart == B && ville_arrivee == C)
-    pthread_mutex_lock(&BC);
-  if(ville_depart == B && ville_arrivee == D)
-    pthread_mutex_lock(&DB);
-  if(ville_depart == C)
-    pthread_mutex_lock(&BC);
-  if(ville_depart == D)
-    pthread_mutex_lock(&DB);
+
+int main(int argc, char* argv[]){
+
+  /* semaphore / Non partagé par les fork /  Valeur de départ */
+  sem_init(&mut,0,CHARGEMAX);
+
+  /* TODO */
+  // pthread_t threads[3];
+  // pthread_create(&threads[0], NULL, TODO, NULL);
+  // pthread_create(&threads[1], NULL, TODO, NULL);
+  // pthread_create(&threads[2], NULL, TODO, NULL);
+  // for (int i = 0; i < 3; i++) {
+    // pthread_join(threads[i], NULL);
+  // }
+
+  return 0;
 }
 
-void liberer_le_segment(int ville_depart, int ville_arrivee)
-{
-  if(ville_depart == A)
-    pthread_mutex_unlock(&AB);
-  if(ville_depart == B && ville_arrivee == C)
-    pthread_mutex_unlock(&BC);
-  if(ville_depart == B && ville_arrivee == D)
-    pthread_mutex_unlock(&DB);
-  if(ville_depart == C)
-    pthread_mutex_unlock(&BC);
-  if(ville_depart == D)
-    pthread_mutex_unlock(&DB);
-}
-
-
-int main(int argc, char* argv[])
-{
-  pthread_t threads[3];
-
-  pthread_create(&threads[0], NULL, train_de_A_vers_C, NULL);
-  pthread_create(&threads[1], NULL, train_de_D_vers_A, NULL);
-  pthread_create(&threads[2], NULL, train_de_A_vers_C, NULL);
-  for (int i = 0; i < 3; i++) {
-    pthread_join(threads[i], NULL);
-  }
-
-}
-
-
-void* train_de_A_vers_C(void* arg)
-{
-	utiliser_le_segment(A, B);
-	printf("Train %d : utilise segment AB \n",(int)pthread_self());
-	attendre(6);
-	liberer_le_segment(A, B);
-
-	utiliser_le_segment(B, C);
-	printf("Train %d : utilise segment BC \n",(int)pthread_self());
-	attendre(6);
-	liberer_le_segment(B, C);
-
-	pthread_exit(NULL);
-}
-
-void* train_de_D_vers_A(void* arg)
-{
-
-	utiliser_le_segment(D, B);
-	printf("Train %d : utilise segment DB \n",(int)pthread_self());
-	attendre(5);
-	liberer_le_segment(D, B);
-
-	utiliser_le_segment(A, B);
-	printf("Train %d : utilise segment BA \n",(int)pthread_self());
-	attendre(5);
-	liberer_le_segment(A, B);
-
-	pthread_exit(NULL);
+/* v = poids */
+void enterPont(int v){
+if(v <= CHARGEMAX){
+  for (int i = 0; i < v; i++) { sem_post(&mut);}
 }
 
 
 
-
-
-int tirage_aleatoire(double max)
-{
-        int j=(int) (max*rand()/(RAND_MAX+1.0));
-        if(j<1)
-                j=1;
-        return j;
 }
-
-
-
-void attendre(double max)
-{
-        struct timespec delai;
-
-        delai.tv_sec=tirage_aleatoire(max);
-        delai.tv_nsec=0;
-        nanosleep(&delai,NULL);
+void ex(){
+  sem_wait(&mut);
+  printf("Meh\n");
+  sem_post(&mut);
 }
